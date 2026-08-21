5361478005f64de10ce5c9e91d9117c534f7c52e
subhasish777 <subhasishk.roy@gmail.com>
2026-08-21 12:48:03 +0530

    feat: migrate to postgres and add web dashboard

--- database.py
+++ database.py
@@ -1,6 +1,11 @@
-import sqlite3
-
-
+import psycopg2
+import os
 
 def get_connection():
-    return sqlite3.connect("servers.db")
+    # Uses environment variables so you don't hardcode credentials in Git
+    return psycopg2.connect(
+        host=os.environ.get("DB_HOST", "localhost"),
+        database=os.environ.get("DB_NAME", "infradb"),
+        user=os.environ.get("DB_USER", "postgres"),
+        password=os.environ.get("DB_PASS", "password")
+    )
